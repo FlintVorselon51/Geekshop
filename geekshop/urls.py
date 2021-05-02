fbdc71dfadbd173b39a51d8afd2f6cb73aa1cf8b
Dmitry <arapov1486@mail.ru>
2021-05-02 18:36:18 +0400

    Working with media.

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -17,10 +17,17 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from mainapp.views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', include('mainapp.urls', namespace='products')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
