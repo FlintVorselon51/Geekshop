57a454135f3f53269a235552bd6effce958844c5
Dmitry <arapov14861@gmail.com>
2021-05-06 13:50:54 +0400

    Authentication and authorization has been added.

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
-# Create your views here.
+from .forms import UserLoginForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'title': 'Geekshop - Авторизация', 'form': form}
+    return render(request, 'authapp/login.html', context=context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
