bd59a56eb48d5413f2ea780c0bca23afa7cc9c61
Dmitry <arapov14861@gmail.com>
2021-05-17 18:09:34 +0400

    profile page and the ability to edit a profile have been added.

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import login, logout, register
+from .views import login, logout, register, profile
 
 app_name = 'authapp'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
-    path('register/', register, name='register')
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile')
 ]
--- authapp/views.py
+++ authapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-from .forms import UserLoginForm, UserRegisterForm
+from .forms import UserLoginForm, UserRegisterForm, UserProfileForm
 
 
 def login(request):
@@ -37,3 +38,21 @@ def register(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            pass
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'title': 'Geekshop - профиль',
+        'form': form
+    }
+    return render(request, 'authapp/profile.html', context)
