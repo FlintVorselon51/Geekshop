05b1c8517cdd5e1d51f3f9cbfc968b33b9a5c5fe
Dmitry <arapov14861@gmail.com>
2021-05-27 15:00:50 +0400

    Added filtering by catalog.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,5 +5,6 @@ from .views import products
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', products, name='index')
+    path('', products, name='index'),
+    path('<int:category_id>/', products, name='product')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -8,10 +8,10 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None):
     context = {
         'title': 'Каталог',
-        'products': Product.objects.all(),
+        'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all(),
         'categories': ProductCategory.objects.all(),
     }
     return render(request, 'mainapp/products.html', context)
